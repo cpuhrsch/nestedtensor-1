b6c5c2955861d41f4a33a81d4916aab8f224a26a
Christian <cpuhrsch@gmail.com>
2020-05-30 00:56:25 -0400

    Checkpoint

--- nestedtensor/csrc/utils/python_nested_node.cpp
+++ nestedtensor/csrc/utils/python_nested_node.cpp
@@ -7,6 +7,8 @@ namespace torch {
 namespace nested_tensor {
 namespace {
 
+// TODO: Need special NestedSize object
+
 template <typename T>
 struct THPNestedNode : torch::CustomClassHolder {
   THPNestedNode(NestedNode<T> size_node, std::string name)
@@ -48,12 +50,41 @@ struct THPNestedNode : torch::CustomClassHolder {
 
 using THPSizeNode = THPNestedNode<c10::List<int64_t>>;
 
+bool eq_size_node(
+    c10::intrusive_ptr<THPSizeNode> a_, 
+    c10::intrusive_ptr<THPSizeNode> b_
+    ) {
+  SizeNode a = a_->get_node();
+  SizeNode b = b_->get_node();
+  if (!shape_matches(a, b)) {
+    return false;
+  }
+  auto fn = [](c10::List<int64_t> a, c10::List<int64_t> b) {
+    for (size_t i = 0; i < a.size(); i++) {
+      if (a[i] != b[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  return all<decltype(fn)>(std::move(fn), a, b);
+}
+
+bool ne_size_node(
+    c10::intrusive_ptr<THPSizeNode> a_, 
+    c10::intrusive_ptr<THPSizeNode> b_
+    ) {
+    return !eq_size_node(a_, b_);
+}
+
 static auto nestedtensor =
     torch::class_<THPSizeNode>("nestedtensor", "SizeNode")
     .def("__str__", &THPSizeNode::str)
     .def("__len__", &THPSizeNode::len)
     .def("unbind", &THPSizeNode::unbind)
     .def("get_payload", &THPSizeNode::get_payload)
+    .def("__eq__", &eq_size_node)
+    .def("__ne__", &ne_size_node)
     ;
 
 c10::intrusive_ptr<THPSizeNode> get_nested_size(at::Tensor self, c10::optional<int64_t> index_) {
--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.0.1.dev20205304+ded6245'
-git_version = 'ded6245adf62c955dc5abe78b98c640ea708cd90'
+__version__ = '0.0.1.dev20205304+1bcc1fb'
+git_version = '1bcc1fbcb6047f82ae3ae591066e330b3666971a'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
