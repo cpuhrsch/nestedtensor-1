5bebd27bebd8e7b9fcb06fb0841a91c38b9a4343
= <=>
2021-07-06 05:10:11 +0000

    Faster transpose - better tiling

--- nestedtensor/version.py
+++ nestedtensor/version.py
@@ -1,5 +1,5 @@
-__version__ = '0.1.4+0759faf'
-git_version = '0759fafed69d5571934a5a71d3226731ffe02429'
+__version__ = '0.1.4+a72a123'
+git_version = 'a72a123e503d4ca927b7eff045f0348e06eeb315'
 from nestedtensor import _C
 if hasattr(_C, 'CUDA_VERSION'):
     cuda = _C.CUDA_VERSION
